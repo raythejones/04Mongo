bee7c1ce7591ee841ff7584129d9f7e65a3a8409
JonathanQuang <jquang@stuy.edu>
2018-02-23 21:56:28 -0500

    can add data to DB

--- 05/popJson.py
+++ 05/popJson.py
@@ -2,8 +2,8 @@ import pymongo
 import json
 
 connection=pymongo.MongoClient("homer.stuy.edu")
-db = connection['jonathanQ-jonesR-db']
-collection = db['jonathanQ-jonesR-collection']
+db = connection['usStats']
+collection = db['usStatsCollection']
 
 
 
@@ -11,7 +11,31 @@ collection = db['jonathanQ-jonesR-collection']
 file = open("populationUSA.json","r")
 data = file.read()
 
-print(data)
+#print(data)
 
-collection.insert_one(data)
+#parse the data
+
+#first remove the starting and ending [{ and }]
+data = data[2:len(data)-2]
+
+#split along }, {
+data = data.split("}, {")
+
+
+
+for index in data:
+    dataDict = {}
+    rowSplit = index.split(', ')
+    for dataPair in rowSplit:
+        colonIndex = dataPair.find(':')
+        theKeyNoQuotes = dataPair[1:colonIndex-1]
+        theValue = dataPair[colonIndex+2:len(dataPair)]
+        theValue = theValue.strip('"')
+        dataDict[theKeyNoQuotes]=theValue
+    print dataDict
+    collection.insert_one(dataDict)
+
+
+
+#collection.insert_one(data)
 
